8dae5d81acc4739e8e69588924b1f6e207d6d6a0
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-12-22 09:14:41 +0100

    first commit

---
+++ dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+let name1 = "Stefan";
+let name2 = "Stefan";
+let isOk = false;
+let numbers = [];
+numbers.push(12);
+//numbers.push("stefanz")
+//isOk = 1
+function bla() {
+    console.log("423423423");
+}
+function calculateTest(a, b) {
+    return a + b;
+}
+let y = calculateTest(12, 11);
+console.log(y);
+let x = 12;
+x = "Kalle";
+console.log("Hello");
---
+++ src/app.ts
@@ -0,0 +1,32 @@
+let name1 :string = "Stefan";
+let name2 :string  = "Stefan";
+
+let isOk: boolean = false;
+
+let numbers: number[] = []
+numbers.push(12)
+//numbers.push("stefanz")
+
+
+//isOk = 1
+
+
+function bla() : void{
+    console.log("423423423")
+}
+
+
+
+function calculateTest(a:number,b:number) : number{
+    return a + b
+}
+
+let y :number = calculateTest(12,11)
+console.log(y)
+
+
+let x:any = 12;
+x = "Kalle"
+
+console.log("Hello");
+
